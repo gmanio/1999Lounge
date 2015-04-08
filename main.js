81744c7c2f3af77276e14258bce0a8f956ce5e76
gman <gmhooray@naver.com>
2015-04-08 17:04:08 +0900

    [TASK] fixed socket Error

--- main.js
+++ main.js
@@ -27,43 +27,64 @@ nts._1999Lounge_ = function () {
 
 nts._1999Lounge_.prototype = {
 	aSocketPool: [],
+	currentLightStatus: [0, 0],
 	Initialize: function () {
 		this.oServer = net.createServer();
 		this.oServer.listen(conf["server"].port);
 		this.request = new net.Socket();
-		this.request.connect(conf["socket"].port, conf["socket"].host, this._modRequestHandler.bind(this));
+		this.request.connect(conf["socket"].port, conf["socket"].host, this._modRequestBuffer.bind(this));
 		this.attachedEvent();
 	},
+
 	attachedEvent: function () {
 		this.oServer.on('connection', this._onServerConnect.bind(this));
 		this.request.on('data', this._modRequestOnData.bind(this));
 	},
-	_onServerConnect: function(oSocket){
+
+	_onServerConnect: function (oSocket) {
 		this.aSocketPool.push(oSocket);
-		console.log(this.aSocketPool.length);
 		oSocket.on('close', function () {
-			var index = this.aSocketPool.indexOf(this);
+			var index = this.aSocketPool.indexOf(oSocket);
 			if (index > -1) {
 				this.aSocketPool.splice(index, 1);
 			}
-		}.bind(this))
+		}.bind(this));
+		oSocket.on('error', function (e) {
+			console.log('error : ' + e);
+		}.bind(this));
 	},
-	_modRequestHandler: function () {
+
+	_modRequestBuffer: function () {
 		var frontLightBuffer = new Buffer(conf["socket"].sElevatorRequestSignal);
 		var backLightBuffer = new Buffer(conf["socket"].sDoorRequestSignal);
 		setInterval(function () {
 			this.request.write(frontLightBuffer);
 			this.request.write(backLightBuffer);
-		}.bind(this),1000);
+		}.bind(this), conf["socket"].requestInterval);
 	},
-	_modRequestOnData: function (data) {
-		if (this.aSocketPool.length >= 2) {
-			this._sendSignalToClient();
+
+	_modRequestOnData: function (bSignals) {
+		var signalDataIndex = bSignals.length - 1;
+		if (bSignals.length == 11) {
+			this.currentLightStatus[0] = bSignals[signalDataIndex];
+		} else if (bSignals.length == 13) {
+			this.currentLightStatus[1] = bSignals[signalDataIndex];
+		}
+
+		if (this.aSocketPool.length >= conf["server"].min_connection) {
+			this._sendSignalToClient(this.currentLightStatus);
 		}
 	},
-	_sendSignalToClient: function(){
-		for(var index in this.aSocketPool){
-			this.aSocketPool[index].write(new Buffer([0]));
+
+	_sendSignalToClient: function (lightStatus) {
+		if (lightStatus[0] == 1 || lightStatus[1] == 1) {
+			for (var index in this.aSocketPool) {
+				this.aSocketPool[index].write(new Buffer([1]));
+			}
+		} else {
+			for (var index in this.aSocketPool) {
+				this.aSocketPool[index].write(new Buffer([0]));
+			}
 		}
 	}
 }
